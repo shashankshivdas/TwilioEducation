e1ba3fb70a94af1bff712b7087c2515810ddc90f
Shashank Shivdas <shashank.shivdas@yahoo.com>
2016-10-19 19:06:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -31,25 +31,25 @@ if($_REQUEST['Body']=='MO'||$_REQUEST['Body']=='mo')
 	}
 if($_REQUEST['Body']=='P'||$_REQUEST['Body']=='p')
 	{        
-	$message1="Our Technician Mark would visit you in the next 24 hours with all the brochures and the best prices for the Projectors\n\n";
+	$message2="Our Technician Mark would visit you in the next 24 hours with all the brochures and the best prices for the Projectors\n\n";
 
 	  $message="$message2";
 	}
 if($_REQUEST['Body']=='M'||$_REQUEST['Body']=='m')
 	{        
-	$message1="Our Technician Jim would visit you in the next 24 hours with all the brochures and the best prices for the Mobile Phones\n\n";
+	$message3="Our Technician Jim would visit you in the next 24 hours with all the brochures and the best prices for the Mobile Phones\n\n";
 
 	  $message="$message3";
 	}
 if($_REQUEST['Body']=='C'||$_REQUEST['Body']=='c')
 	{        
-	$message1="Our Technician Mike would visit you in the next 24 hours with all the brochures and the best prices for the CPUs\n\n";
+	$message4="Our Technician Mike would visit you in the next 24 hours with all the brochures and the best prices for the CPUs\n\n";
 
 	  $message="$message4";
 	}
 if($_REQUEST['Body']=='W'||$_REQUEST['Body']=='w')
 	{        
-	$message1="Our Technician Luke would visit you in the next 24 hours with all the brochures and the best prices for the Computers\n\n";
+	$message5="Our Technician Luke would visit you in the next 24 hours with all the brochures and the best prices for the Computers\n\n";
 
 	  $message="$message5";
 	}
@@ -73,25 +73,25 @@ if($_REQUEST['Body']=='MO'||$_REQUEST['Body']=='mo')
 	}
 if($_REQUEST['Body']=='P'||$_REQUEST['Body']=='p')
 	{        
-	$message1="Our Technician Mark would visit you in the next 48 hours to check for the issue with the Projectors\n\n";
+	$message2="Our Technician Mark would visit you in the next 48 hours to check for the issue with the Projectors\n\n";
 
 	  $message="$message2";
 	}
 if($_REQUEST['Body']=='M'||$_REQUEST['Body']=='m')
 	{        
-	$message1="Our Technician Jim would visit you in the next 48 hours to check for the issue with the Mobile Phones\n\n";
+	$message3="Our Technician Jim would visit you in the next 48 hours to check for the issue with the Mobile Phones\n\n";
 
 	  $message="$message3";
 	}
 if($_REQUEST['Body']=='C'||$_REQUEST['Body']=='c')
 	{        
-	$message1="Our Technician Mike would visit you in the next 48 hours to check for the issue with the CPUs\n\n";
+	$message4="Our Technician Mike would visit you in the next 48 hours to check for the issue with the CPUs\n\n";
 
 	  $message="$message4";
 	}
 if($_REQUEST['Body']=='W'||$_REQUEST['Body']=='w')
 	{        
-	$message1="Our Technician Luke would visit you in the next 48 hours to check for the issue with the Monitors Computers\n\n";
+	$message5="Our Technician Luke would visit you in the next 48 hours to check for the issue with the Monitors Computers\n\n";
 
 	  $message="$message5";
 	}
@@ -101,7 +101,7 @@ if($_REQUEST['Body']=='W'||$_REQUEST['Body']=='w')
     <Message>
 Hello  <?php echo $number?>.
 <?php echo $message?> 
-<?php echo"\nPlease type Start to redo the procedure";?>	    
+<?php echo"\nPlease type Start to redo the procedure";?>    
 
 </Message>
 </Response>
